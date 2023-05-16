baf7016e65e0314c872fdde916ebfb22eafa6dd8
HarveyBurton96 <>
2023-05-16 14:20:13 +0000

    Added text to the summary page

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -1,4 +1,33 @@
 import streamlit as st
 
+
 def page_summary_body():
-    st.write("This is summary page")
+    st.header("Project Summary")
+
+    st.info(
+        f"Project Datasets\n\n"
+        f"The dataset we are using represents the sale conditions of one house"
+        f"contains overall condition of the property (like the Rating of "
+        f"basement finished area, Interior finish of the garage, Kitchen "
+        f"quality and Overall condition of the house) the year constructed "
+        f"(like the Remodel date, Original construction date and the Year "
+        f"garage was built) it also contians the square feet of the property "
+        f"(like the First Floor square feet, Second-floor square feet and "
+        f"Total square feet of basement area"
+    )
+
+    st.warning(
+        f"Business requirement 1:\n\n"
+        f"The client is interested in discovering how the house attributes "
+        f"correlate with the sale price. Therefore, the client expects data "
+        f"visualizations of the correlated variables against the sale price "
+        f"to show that.\n\n"
+        f"Business requirement 2:\n\n"
+        f"The client is interested in predicting the house sales price from "
+        f"her four inherited houses, and any other house in Ames, Iowa.\n"
+    )
+
+    st.write(
+        f"For additional information about this project please read the "
+        f"[Project README.](https://github.com/HarveyBurton96/milestone-project-heritage-housing-issues)"
+    )
