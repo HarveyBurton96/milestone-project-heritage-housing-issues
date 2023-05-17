1241a0ca106b8329a884d937201e0d817b1eeffd
HarveyBurton96 <>
2023-05-17 19:16:01 +0000

    Set up Sale price piepline and loaded first inherited house

--- app_pages/page_inherited_house_prices.py
+++ app_pages/page_inherited_house_prices.py
@@ -1,5 +1,34 @@
 import streamlit as st
+import pandas as pd
+from src.predictive_model import predict_sale_price
+from src.data_management import inherit_house_data, load_pkl_file
 
 
 def page_inherited_body():
-    st.write("This is inherited page")
+
+    df = inherit_house_data()
+    house_one = df.iloc[[0]]
+    house_two = df.iloc[[1]]
+    house_three = df.iloc[[2]]
+    house_four = df.iloc[[3]]
+
+    features = ['OverallQual', 'TotalBsmtSF', '2ndFlrSF', 'GarageArea']
+
+    version = 'v1'
+
+    sale_price_pipe = load_pkl_file(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/regressor_pipeline.pkl"
+    )
+
+    predict_house_one = predict_sale_price(
+        house_one, features, sale_price_pipe)
+
+    st.write(house_one)
+
+    st.write(house_two)
+
+    st.write(house_three)
+
+    st.write(house_four)
+
+    st.header("Four Inherited Houses Sale Prices")
--- src/data_management.py
+++ src/data_management.py
@@ -9,5 +9,10 @@ def load_house_data():
     return df
 
 
+def inherit_house_data():
+    df_inherit = pd.read_csv("inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/inherited_houses.csv")
+    return df_inherit
+
+
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
